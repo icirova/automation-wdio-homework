3c35b9a1734c733292fbd6e29590a9b9b755862b
icirova <cirova.iveta@ptw.cz>
2022-12-14 20:28:13 +0100

    Prvni komit

--- specs/order.e2e.js
+++ specs/order.e2e.js
@@ -10,8 +10,45 @@ import {
     endDate
 } from './fixtures.js'
 
+import {
+    getFieldValueById
+} from '../pages/functions.js'
+
 describe('Objednávka pro MŠ/ZŠ', () => {
 
-    // testy sem :)
+    beforeEach(() => {
+        browser.reloadSession()
+        browser.url('/objednavka/pridat')
+    })
 
-});
+    it(
+        'Vyplnění objednávkového formuláře', () => {
+
+            $('#ico').setValue(ICO)
+            browser.keys('Enter')
+
+            browser.pause(4000)
+
+            const value = getFieldValueById('client')
+
+            console.log('Value: ' + value)
+
+            const address = getFieldValueById('address')
+
+            console.log('Value: ' + address)
+
+            $('#substitute').setValue(substituteName)
+            $('#contact_name').setValue(contactName)
+            $('#contact_tel').setValue(contactPhone)
+            $('#contact_mail').setValue(contactEmail)
+            $('#start_date_1').setValue(startDate)
+            $('#end_date_1').setValue(endDate)
+
+            $('#nav-home-tab').click()
+            $('#camp-date_part').selectByVisibleText('Odpolední')
+
+            //browser.saveScreenshot('foto.png')
+        }
+    )
+
+})
