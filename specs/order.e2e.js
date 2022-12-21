60bf384f4f1eec9c086b4ad91aa9860c5016ccf7
icirova <cirova.iveta@ptw.cz>
2022-12-21 16:22:38 +0100

    funkce

--- specs/order.e2e.js
+++ specs/order.e2e.js
@@ -14,6 +14,74 @@ import {
     getFieldValueById
 } from '../pages/functions.js'
 
+function getIco() {
+    return $('#ico')
+}
+
+function getToast() {
+    return $('.toast-message')
+}
+
+function getClientName(){
+    return $('#client')
+}
+
+function getAddress(){
+    return $('#address')
+}
+
+function getSubstitute() {
+    return $('#substitute')
+}
+
+function getName() {
+    return $('#contact_name')
+}
+function getTel() {
+    return $('#contact_tel')
+}
+function getMail() {
+    return $('#contact_mail')
+}
+ function getBeginning() {
+    return $('#start_date_1')
+ }
+function getEnd() {
+    return $('#end_date_1')
+}
+function getCampButton () {
+    return $('#nav-home-tab')
+}
+function getDayTimeCamp() {
+    return $('#camp-date_part')
+}
+
+function getStudents () {
+    return $('#camp-students')
+}
+function getAge() {
+    return $('#camp-age')
+}
+function getAdults() {
+    return $('#camp-adults')
+}
+function getSave(){
+    return $('.btn-primary')
+}
+function getConfirmation() {
+    return $('h3=Děkujeme za objednávku')
+}
+function getOrder () {
+    return $('h3*=Objednávka akce')
+}
+function getTeachers(){
+    return  $('a=Pro učitelé')
+}
+
+function getSchool() {
+    return $('a=Objednávka pro MŠ/ZŠ')
+}
+
 describe('Objednávka pro MŠ/ZŠ', () => {
 
     beforeEach(() => {
@@ -23,22 +91,20 @@ describe('Objednávka pro MŠ/ZŠ', () => {
 
     it(
         'Vyplnění IČO a automatické načtení jména a adresy odběratele z ARESu', () => {
-
-            const ico = $('#ico')
-            const client = $('#client')
-            const inputAddress = $('#address')
-
+                     
+            const ico = getIco()
             ico.setValue(ICO)
             browser.keys('Enter')
 
-            const toast = $('.toast-message')
+            const toast = getToast()
             toast.waitForExist()
 
+            const client = getClientName()
             const value = getFieldValueById('client')
             expect(client).toBeDisplayed()
             expect(value).toEqual(clientName)
 
-
+            const inputAddress = getAddress()
             const address = getFieldValueById('address')
             expect(inputAddress).toBeDisplayed()
             expect(clientAddress).toEqual(address)
@@ -46,88 +112,102 @@ describe('Objednávka pro MŠ/ZŠ', () => {
         }
     )
 
+
+
     it(
         'Vyplnění a následné odeslání objednávky', () => {
 
-            const ico = $('#ico')
-            const substitute = $('#substitute')
-            const name = $('#contact_name')
-            const tel = $('#contact_tel')
-            const mail = $('#contact_mail')
-            const beginning = $('#start_date_1')
-            const end = $('#end_date_1')
-            const campButton = $('#nav-home-tab')
-            const dayTimeCamp = $('#camp-date_part')
-            const students = $('#camp-students')
-            const age = $('#camp-age')
-            const adults = $('#camp-adults')
-            const save = $('.btn-primary')
-            const toastMessage = $('.toast-message')
-            const confirmation = $('h3=Děkujeme za objednávku')
-
+            const ico = getIco()
             ico.setValue(ICO)
 
+            const substitute = getSubstitute()
             substitute.setValue(substituteName)
+
+            const name = getName()
             name.setValue(contactName)
+
+            const tel = getTel()
             tel.setValue(contactPhone)
+
+            const mail = getMail()
             mail.setValue(contactEmail)
+
+            const beginning = getBeginning()
             beginning.setValue(startDate)
+
+            const end = getEnd()
             end.setValue(endDate)
 
+            const campButton = getCampButton()
             campButton.click()
 
+            const dayTimeCamp =getDayTimeCamp()
             dayTimeCamp.selectByVisibleText('Odpolední')
+            
+            const students =getStudents ()
             students.setValue(20)
+            
+            const age = getAge()
             age.setValue(12)
+            
+            const adults = getAdults()
             adults.setValue(2)
+            
+            const save = getSave()
             save.click()
 
+            const confirmation = getConfirmation()
             expect(confirmation).toBeDisplayed
-            expect(toastMessage.getText()).toEqual('Objednávka byla úspěšně uložena')
+            expect(getToast().getText()).toEqual('Objednávka byla úspěšně uložena')
 
         }
     )
 
     it(
         'Objednávku nelze odeslat pokud není řádně vyplněna', () => {
-
-            const client = $('#client')
-            const completeAddress = $('#address')
-            const ico = $('#ico')
-            const substitute = $('#substitute')
-            const name = $('#contact_name')
-            const tel = $('#contact_tel')
-            const mail = $('#contact_mail')
-            const beginning = $('#start_date_1')
-            const end = $('#end_date_1')
-            const campButton = $('#nav-home-tab')
-            const dayTimeCamp = $('#camp-date_part')
-            const students = $('#camp-students')
-            const age = $('#camp-age')
-            const adults = $('#camp-adults')
-            const save = $('.btn-primary')
-            const confirmation = $('h3=Děkujeme za objednávku')
-
-            client.setValue(clientName)
-            completeAddress.setValue(clientAddress)
+             
+            const ico = getIco()
             ico.setValue(ICO)
 
+            const substitute = getSubstitute()
             substitute.setValue(substituteName)
+
+            const name = getName()
             name.setValue(contactName)
+            
+            const tel = getTel()
             tel.setValue(contactPhone)
+            
+            const mail = getMail()
             mail.setValue(contactEmail)
+            
+            const beginning = getBeginning()
             beginning.setValue()
+            
+            const end = getEnd()
             end.setValue()
 
+            const campButton = getCampButton()
             campButton.click()
+            
+            const dayTimeCamp = getDayTimeCamp()
             dayTimeCamp.selectByVisibleText('Odpolední')
+            
+            const students = getStudents()
             students.setValue(20)
+            
+            const age = getAge()
             age.setValue(12)
+
+            const adults = getAdults()
             adults.setValue(2)
 
+            const save = getSave()
             save.click()
 
+            const confirmation = getConfirmation()
             expect(confirmation).not.toBeDisplayed()
+            
             expect(save).toBeDisplayed()
 
         }
@@ -136,19 +216,28 @@ describe('Objednávka pro MŠ/ZŠ', () => {
 
 })
 
+
+
 describe('Navigace', () => {
 
-    it(
+    beforeEach(() => {
+        browser.reloadSession()
+        browser.url('')
+    })
+    
+    it.only(
         'Funkční navigace', () => {
 
-            const substitute = $('#substitute')
-            const order = $('h3*=Objednávka akce')
+            const teachers = getTeachers()
+            teachers.click()
+            
+            const school = getSchool()
+            school.click()
 
-            browser.url('')
-            $('a=Pro učitelé').click()
-            $('a=Objednávka pro MŠ/ZŠ').click()
-
+            const order = getOrder()
             expect(order).toBeDisplayed()
+            
+            const substitute = getSubstitute()
             expect(substitute).toBeDisplayed()
 
         }
